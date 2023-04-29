e200774f3de3cb199021072ad97efac6dd27f1f7
joeun <thdwhdms99@naver.com>
2023-04-29 19:03:01 +0900

    Feat: navigation bar(#1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "../src/styles/Global.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserNav from "./components/navigation/UserNav";
+import Test from "./pages/Test";
+import AdminNav from "./components/navigation/AdminNav";
 
 function App() {
-  return <div className="App">하이</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route element={<UserNav />}>
+            <Route path="/" element={<Test />} />
+          </Route>
+        </Routes>
+
+        <Routes>
+          <Route element={<AdminNav />}>
+            <Route path="/admin/main" element={<Test />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
