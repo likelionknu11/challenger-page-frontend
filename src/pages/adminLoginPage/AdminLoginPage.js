d7ff816e385702d8fd3b79b2c8eca842b21745da
joeun <thdwhdms99@naver.com>
2023-05-01 22:43:25 +0900

    Feat: loginPage 로그인 성공/실패 구현 (#5)

--- src/pages/adminLoginPage/AdminLoginPage.js
+++ src/pages/adminLoginPage/AdminLoginPage.js
@@ -1,8 +1,38 @@
-import React from "react";
-import LoginBox from "./LoginStyles";
+import React, { useState } from "react";
+import LoginBox, { InputBox, LoginBtn, Logo } from "./LoginStyles";
+import { useNavigate } from "react-router-dom";
 
 const AdminLoginPage = () => {
-  return <LoginBox />;
+  const navigate = useNavigate();
+  const [id, setId] = useState("");
+  const [pw, setPw] = useState("");
+  const realId = "aaaa";
+  const realPw = "aaaa";
+
+  const handleChange = e => {
+    if (e.target.name === "id") {
+      setId(e.target.value);
+    }
+    if (e.target.name === "pw") {
+      setPw(e.target.value);
+    }
+  };
+
+  const handleClick = () => {
+    if (id === realId && pw === realPw) {
+      navigate("/admin/main");
+    } else {
+      alert("잘못된 접근입니다!");
+    }
+  };
+  return (
+    <LoginBox>
+      <Logo />
+      <InputBox type="id" onchange={handleChange} />
+      <InputBox type="pw" onchange={handleChange} />
+      <LoginBtn onClick={handleClick}>강남멋사 관리자 로그인</LoginBtn>
+    </LoginBox>
+  );
 };
 
 export default AdminLoginPage;
