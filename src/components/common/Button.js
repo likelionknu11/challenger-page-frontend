1fc61564dde9cf51c9fb42541ca4eddca2670376
jieun <jieunc023@gmail.com>
2023-05-03 22:05:14 +0900

    Feature: button(#9)

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,24 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 
-export default function Button({ color, backColor, text }) {
+export default function Button({ btnColor, children, onClick }) {
+  const color = { blue: "#ffffff", gray: "#2B2B2B" };
+  const backColor = { blue: "#4F85EB", gray: "D9D9D9" };
+  const btn = css`
+    width: 5em;
+    height: 2.5em;
+    color: ${color[btnColor]};
+    background-color: ${backColor[btnColor]};
+    font-family: "Noto Sans";
+    font-style: normal;
+    font-weight: 700;
+    font-size: 1.25em;
+    text-align: center;
+  `;
   return (
     <div>
-      <button
-        css={css`
-          width: 5em;
-          height: 2.5em;
-          color: ${color};
-          background-color: ${backColor};
-          font-family: roboto;
-          font-style: normal;
-          font-weight: 700;
-          font-size: 1.25em;
-          line-height: 28px;
-          text-align: center;
-        `}
-      >
-        {text}
+      <button css={btn} onClick={onClick}>
+        {children}
       </button>
     </div>
   );
