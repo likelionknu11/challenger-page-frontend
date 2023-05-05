fb3e1ebce4a5a9e56091b0d79768679736a84b6d
joeun <thdwhdms99@naver.com>
2023-05-05 16:19:07 +0900

    Feat: loginPage 인증 전역상태 생성 (#5)

--- src/components/navigation/AdminNav.js
+++ src/components/navigation/AdminNav.js
@@ -1,9 +1,16 @@
 // 관리자 네비게이션바
-import React from "react";
+import React, { useEffect } from "react";
 import { AdNav, Logo } from "./NavStyle";
 import { Link } from "react-router-dom";
+import { auth } from "../store";
 
 const AdminNav = () => {
+  const { isAuth } = auth();
+  localStorage.setItem("isAuthStorage", isAuth ? isAuth : undefined);
+  console.log(localStorage.getItem("isAuthStorage"));
+
+  useEffect = () => {};
+
   return (
     <AdNav>
       <Link to="/admin/main">
