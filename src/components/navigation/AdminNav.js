ca7af95fc895bd799d81eed2c688b58a90afc691
joeun <thdwhdms99@naver.com>
2023-05-06 14:43:49 +0900

    Feat: navigation bar - 임시임시 커밋 (#1)

--- src/components/navigation/AdminNav.js
+++ src/components/navigation/AdminNav.js
@@ -1,15 +1,15 @@
 // 관리자 네비게이션바
 import React from "react";
-import { AdNav, Logo } from "./Nav";
+import { AdNav, Logo, Nav } from "./Nav";
 import { Link } from "react-router-dom";
 
 const AdminNav = () => {
   return (
-    <AdNav>
+    <Nav>
       <Link to="/admin/main">
         <Logo>로고</Logo>
       </Link>
-    </AdNav>
+    </Nav>
   );
 };
 
